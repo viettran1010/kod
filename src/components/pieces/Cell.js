3a610536f90579fbeeedfa34db97d39f0af5f12e
viettran1010 <48788462+viettran1010@users.noreply.github.com>
2020-10-28 14:39:25 +0700

    Update Cell.js

--- src/components/pieces/Cell.js
+++ src/components/pieces/Cell.js
@@ -4,13 +4,14 @@ import styles from './Cell.module.css'
 export const Cell = (props) => {
     const colors = ["green", "red", "cyan", "yellow", "purple"];
     const background = colors[props.backgroundId]
-    // console.log(background);
     return (
         <div
             className={styles.cell}
-            style={{ backgroundColor: `${background}` }}
-            onClick={() => { props.swapColor(props.row, props.col) }}
-        >
+            style={{ backgroundColor: `${background}`,
+            transition: "all .1s ease",
+            WebkitTransition: "all 1s ease",
+            MozTransition: "all 1s ease"}}
+            onClick={() => { props.swapColor(props.row, props.col) }}>
         </div>
     )
 }
